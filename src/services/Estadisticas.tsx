2403ac01ec7e08c1581a532f45a59b7834f8b95e
Checho0507 <checho.mlj@gmail.com>
2025-05-15 19:29:24 -0500

    Cambio en el servicio de estadisticas

--- src/services/Estadisticas.tsx
+++ src/services/Estadisticas.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000"; // Ajusta si cambias de entorno
+const API_BASE_URL = "https://backendgesex-production.up.railway.app"; // Ajusta si cambias de entorno
 
 // 1. Crear estadística
 
