8df8780cfd89b3bc39c5fb830abc56ef75c658d3
Checho0507 <checho.mlj@gmail.com>
2026-02-11 20:21:06 -0500

    eliminacion de cosas en el footer y header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <nav className="bg-red-700 text-white px-6 py-2 flex justify-between items-center">
       <Link to="/">
-        <img src="/logo.png" alt="Logo" className="h-20" />
+        {/*<img src="/logo.png" alt="Logo" className="h-20" />*/}
       </Link>
       <ul className="flex gap-6 text-lg"> {/* text-lg para que no se vea tan grande */}
         <li><Link to="/encuesta" className="hover:underline">Encuesta</Link></li>
-        <li><Link to="/estadisticas" className="hover:underline">Estadísticas</Link></li>
-        <li><Link to="/material" className="hover:underline">Material de apoyo</Link></li>
+        {/*<li><Link to="/estadisticas" className="hover:underline">Estadísticas</Link></li>
+        <li><Link to="/material" className="hover:underline">Material de apoyo</Link></li>*/}
       </ul>
       <User className="w-12 h-12" />
     </nav>
